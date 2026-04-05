b495e79a68df85c8d2e79714f9c3de37d21aee58
Shlomis23 <your@email.com>
2026-04-05 22:17:14 +0300

    Fix vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/booking-app/' : '/',
   server: { port: 5173 },
-}));
+});
